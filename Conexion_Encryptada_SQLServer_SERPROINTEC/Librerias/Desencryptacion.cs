b9fb360410fb57ff9406c5d1378008e9ab8af706
serprointec <guamanzh@gmail.com>
2023-09-11 11:46:39 -0500

    Clases Encryptacion-Descryptacion

---
+++ Conexion_Encryptada_SQLServer_SERPROINTEC/Librerias/Desencryptacion.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//se usa la siguiente libreria 
+using System.Xml;
+
+
+namespace Conexion_Encryptada_SQLServer_SERPROINTEC.Librerias
+{
+    internal class Desencryptacion
+    {
+        static private AES aes = new AES();
+        static public string CnString;
+        static string dbcnString;
+        static public string appPwdUnique = "SERPROINTEC.COM.RESTAURANTEAYANA.Hol@_Mundo2024";
+
+        public static object checkServer()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load("ConnectionString.xml");
+            XmlElement root = doc.DocumentElement;
+            dbcnString = root.Attributes[0].Value;
+            CnString = (aes.Decrypt(dbcnString, appPwdUnique, int.Parse("256")));
+            return CnString;
+
+        }
+
+        internal class label
+        {
+
+        }
+        public static object UsuariosEncryp()
+        {
+            XmlDocument doc = new XmlDocument();
+            label root = new label();
+
+            dbcnString = root.ToString();
+            CnString = (aes.Decrypt(dbcnString, appPwdUnique, int.Parse("256")));
+            return CnString;
+
+        }
+    }
+}
