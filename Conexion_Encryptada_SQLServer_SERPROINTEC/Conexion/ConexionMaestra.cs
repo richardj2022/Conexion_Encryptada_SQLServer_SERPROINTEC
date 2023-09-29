37705855ef7e8826fe5c8ce1d46f08c849fbdbac
serprointec <guamanzh@gmail.com>
2023-09-28 22:02:53 -0500

    Actualizacion de proyecto

--- Conexion_Encryptada_SQLServer_SERPROINTEC/Conexion/ConexionMaestra.cs
+++ Conexion_Encryptada_SQLServer_SERPROINTEC/Conexion/ConexionMaestra.cs
@@ -13,7 +13,5 @@ namespace Conexion_Encryptada_SQLServer_SERPROINTEC.Conexion
     internal class ConexionMaestra
     {
         public static string conexion = Convert.ToString(Librerias.Desencryptacion.checkServer());
-
-
     }
 }
